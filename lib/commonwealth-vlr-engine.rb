5a71c6ef2e01deb4b1b4e0156ec036038bd3774c
Eben English <eenglish@bpl.org>
2015-06-22 12:07:42 -0400

    move all mlt constraints stuff into CommonwealthVlrEngine::RenderConstraintsOverride

--- lib/commonwealth-vlr-engine.rb
+++ lib/commonwealth-vlr-engine.rb
@@ -39,13 +39,12 @@ module CommonwealthVlrEngine
     CatalogController.send(:include, CommonwealthVlrEngine::RenderConstraintsOverride)
     CatalogController.send(:helper, CommonwealthVlrEngine::RenderConstraintsOverride) unless
         CatalogController.helpers.is_a?(CommonwealthVlrEngine::RenderConstraintsOverride)
-=begin
-    # TODO inject into SearchHistory and SavedSearches so mlt queries display properly
+
+    # inject into SearchHistory and SavedSearches so mlt queries display properly
     SearchHistoryController.send(:helper, CommonwealthVlrEngine::RenderConstraintsOverride) unless
         SearchHistoryController.helpers.is_a?(CommonwealthVlrEngine::RenderConstraintsOverride)
     SavedSearchesController.send(:helper, CommonwealthVlrEngine::RenderConstraintsOverride) unless
         SavedSearchesController.helpers.is_a?(CommonwealthVlrEngine::RenderConstraintsOverride)
-=end
   end
 
 end
