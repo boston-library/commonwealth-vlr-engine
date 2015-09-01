62fc92aaf2bd7c8876c8bebf42083bc1dcfa5e45
Eben English <eenglish@bpl.org>
2015-09-01 16:34:39 -0400

    setting up test env using blacklight-maps patterns

---
+++ lib/railties/commonwealth_vlr_engine.rake
@@ -0,0 +1,13 @@
+require 'rails/generators'
+require 'generators/commonwealth_vlr_engine/install_generator'
+
+namespace :commonwealth_vlr_engine do
+  namespace :solr do
+    desc "Put sample data into solr"
+    task :seed => :environment do
+      docs = YAML::load(File.open(File.expand_path(File.join('..', '..', '..', 'spec', 'fixtures', 'sample_solr_documents.yml'), __FILE__)))
+      Blacklight.solr.add docs
+      Blacklight.solr.commit
+    end
+  end
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,36 @@
+ENV["RAILS_ENV"] ||= 'test'
+
+require 'engine_cart'
+#require 'coveralls'
+#Coveralls.wear!('rails')
+EngineCart.load_application!
+
+require 'capybara/poltergeist'
+Capybara.javascript_driver = :poltergeist
+
+Capybara.register_driver :poltergeist do |app|
+  options = {}
+
+  options[:timeout] = 120 if RUBY_PLATFORM == "java"
+
+  Capybara::Poltergeist::Driver.new(app, options)
+end
+
+if ENV["COVERAGE"] or ENV["CI"]
+  require 'simplecov'
+
+  SimpleCov.formatter = Coveralls::SimpleCov::Formatter
+  SimpleCov.start do
+    add_filter "/spec/"
+  end
+end
+
+require 'commonwealth-vlr-engine'
+
+require 'rspec/rails'
+require 'capybara/rspec'
+
+
+RSpec.configure do |config|
+  config.infer_spec_type_from_file_location!
+end
