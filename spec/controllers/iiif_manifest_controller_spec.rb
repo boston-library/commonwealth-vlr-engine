5b93a4fddb27ca0495b77126b867077fcb273424
Eben English <eenglish@bpl.org>
2015-09-08 16:58:03 -0400

    add basic specs for IiifManifestController :canvas and :annotation actions

--- spec/controllers/iiif_manifest_controller_spec.rb
+++ spec/controllers/iiif_manifest_controller_spec.rb
@@ -24,4 +24,43 @@ describe IiifManifestController do
 
   end
 
+  describe 'get canvas' do
+
+    before(:each) do
+      get :canvas, :id => 'bpl-dev:h702q6403', :canvas_object_id => 'bpl-dev:h702q641c'
+      @response_body = JSON.parse(response.body)
+    end
+
+    it 'should render an IIIF canvas' do
+      expect(response).to be_success
+      expect(@response_body).to have_content('http://iiif.io/api/presentation/2/context.json')
+    end
+
+    it 'should conform to the IIIF manifest spec' do
+      expect(@response_body).to have_content('h702q6403/canvas/h702q641c')
+      expect(@response_body["images"].length).to eq(1)
+      expect(@response_body["images"].first["resource"]["@id"]).to have_content('h702q641c')
+    end
+
+  end
+
+  describe 'get annotation' do
+
+    before(:each) do
+      get :annotation, :id => 'bpl-dev:h702q6403', :annotation_object_id => 'bpl-dev:h702q641c'
+      @response_body = JSON.parse(response.body)
+    end
+
+    it 'should render an IIIF annotation' do
+      expect(response).to be_success
+      expect(@response_body).to have_content('http://iiif.io/api/presentation/2/context.json')
+    end
+
+    it 'should conform to the IIIF manifest spec' do
+      expect(@response_body).to have_content('h702q6403/annotation/h702q641c')
+      expect(@response_body["resource"]["@id"]).to have_content('h702q641c')
+    end
+
+  end
+
 end
