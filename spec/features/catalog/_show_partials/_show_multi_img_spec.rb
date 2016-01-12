0b014f1a29d1bb54b1452bf328e7d5e0aa3ddd41
Eben English <eenglish@bpl.org>
2016-01-12 09:22:48 -0500

    basic specs for book viewer search features

--- spec/features/catalog/_show_partials/_show_multi_img_spec.rb
+++ spec/features/catalog/_show_partials/_show_multi_img_spec.rb
@@ -1,15 +1,5 @@
 require 'spec_helper'
 
-describe 'openseadragon image viewer modal' do
-
-  it 'should display the OSd viewer modal when the image is clicked', :js => true do
-    visit solr_document_path(:id => 'bpl-dev:h702q6403')
-    click_link('img_viewer_link')
-    expect(page).to have_selector('.openseadragon-container')
-  end
-
-end
-
 describe 'multi image viewer' do
 
   before(:each) do
---
+++ spec/features/image_viewer/book_viewer_spec.rb
@@ -0,0 +1,67 @@
+require 'spec_helper'
+
+describe 'book_viewer', js: true do
+
+  let(:book_pid) { 'bpl-dev:7s75dn48d' }
+  let(:ocr_search_param) { 'ocr_q' }
+  let(:ocr_search_term) { 'instruction' }
+
+  describe 'search behavior' do
+
+    before do
+      visit book_viewer_path(:id => book_pid)
+      click_link('toggle-search')
+    end
+
+    it 'should render the search inside partial as a Bootstrap modal within the page' do
+      expect(page).to have_selector('.modal-header')
+      expect(page).to have_selector('.blacklight-image_viewer')
+    end
+
+    describe 'running a search' do
+
+      before do
+        within 'form.ocr-search-form' do
+          fill_in ocr_search_param, with: ocr_search_term
+          click_button('search')
+        end
+      end
+
+      it 'should display the results in the modal window within the page' do
+        expect(page).to have_selector('.modal-header')
+        expect(page).to have_selector('.blacklight-image_viewer')
+      end
+
+      describe 'linking to page' do
+
+        before do
+          click_link('Page 5')
+        end
+
+        it 'should go to the correct page' do
+          expect(page).to have_selector('div.page.current img[src*="bpl-dev:7s75dn59x"]')
+        end
+
+        it 'should update the URL' do
+          expect(current_url).to include("#{ocr_search_param}=#{ocr_search_term}#1/2")
+        end
+
+        describe 'starting a new search' do
+
+          before do
+            click_link('toggle-search')
+          end
+
+          it 'should display the results for the most recent search' do
+            expect(page).to have_selector('#ocr_search_details')
+          end
+
+        end
+
+      end
+
+    end
+
+  end
+
+end
