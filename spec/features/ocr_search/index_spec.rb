24f731f8fca8b1f4552693c434667eb58625eea0
Eben English <eenglish@bpl.org>
2016-01-15 12:06:34 -0500

    specs for term freq and snippets visibility toggle

--- spec/features/ocr_search/index_spec.rb
+++ spec/features/ocr_search/index_spec.rb
@@ -29,18 +29,40 @@ describe 'OCR search index view' do
 
       before do
         within 'form.ocr-search-form' do
-          fill_in 'ocr_q', with: 'instruction'
+          fill_in 'ocr_q', with: 'the'
           click_button('search')
         end
+      end
+
+      it 'should display the sort widget' do
+        expect(page).to have_selector('#sort-dropdown')
       end
 
       it 'should display the search results' do
         expect(page).to have_selector('.ocr_search_result', count: 2)
       end
 
-      it 'should display the search term highlighting and page link' do
+      it 'should display the page link, snippets, term frequency, and more matches' do
         expect(page).to have_selector('.ocr_page_link', count: 2)
-        expect(page).to have_selector('.ocr_snippet', count: 2)
+        expect(page).to have_selector('.first_snippet', count: 2)
+        expect(page).to have_selector('.ocr_term_freq', count: 2)
+        expect(page).to have_selector('.ocr_snippets_expand', count: 2)
+      end
+
+      it 'should have hidden snippets' do
+        expect(page).to have_selector('#snippet_collapse_1', visible: false)
+      end
+
+      describe 'show hidden snippets' do
+
+        before do
+          page.find("a[href*='#snippet_collapse_1']").click
+        end
+
+        it 'should display more snippets' do
+          expect(page).to have_selector('#snippet_collapse_1', visible: true)
+        end
+
       end
 
     end
