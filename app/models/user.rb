459313438f8440800e25fbb6d2a359f3aa07e813
Steven Anderson <sanderson@bpl.org>
2016-04-15 12:22:13 -0400

    Move devise dependency to bpluser and add custom validations to the user class

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,16 +3,11 @@ class User < ActiveRecord::Base
   require 'cgi'
   require 'openssl'
   require 'rest_client'
-  require 'devise'
 
   # Connects this user object to Blacklights Bookmarks.
   include Blacklight::User
 
   # Connects this user object to the BPL omniauth service
-  include Bpluser::User
-
-  # Include our custom validations
-  include Bpluser::Validatable
 
   self.table_name = "users"
 
