d887c64679015c037d1e0cf296c78f6b85479e39
Steven Anderson <sanderson@bpl.org>
2016-06-24 15:52:48 -0400

    OVerride the blacklight show helper in a different fashion

--- lib/commonwealth-vlr-engine/blacklight_override.rb
+++
@@ -1,8 +0,0 @@
-# Meant to be applied on top of CatalogController to override some instance methods
-module CommonwealthVlrEngine
-  module BlacklightOverride
-    def render_bookmarks_control?
-      false
-    end
-  end
-end
