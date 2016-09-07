8020095d80f8b051475c1876f29d18bedcc1d64f
Eben English <eenglish@bpl.org>
2016-09-07 13:49:59 -0400

    clean up module for zipline-based zipped downloads

--- app/controllers/concerns/commonwealth_vlr_engine/downloads_controller_behavior.rb
+++ app/controllers/concerns/commonwealth_vlr_engine/downloads_controller_behavior.rb
@@ -7,25 +7,22 @@ module CommonwealthVlrEngine
     include CommonwealthVlrEngine::Streaming
     include CommonwealthVlrEngine::ApplicationHelper
     include CommonwealthVlrEngine::Finder
-    # for some reason have to require AND include Zipline, or you get 'uninitialized constant' errors
+    # for some reason have to require AND include Zipline, or you get errors
     require 'zipline'
     include Zipline
     require 'open-uri'
 
     # Responds to http requests to show the file
     def show
-      @zip = false
       @solr_document = fetch(params[:id])[1]
-      if !@solr_document.empty?
+      if !@solr_document.empty? && params[:datastream_id]
         if @solr_document[:has_model_ssim].include? 'info:fedora/afmodel:Bplmodels_File'
           send_content
         elsif @solr_document[:has_model_ssim].include? 'info:fedora/afmodel:Bplmodels_ObjectBase'
           @file_list = get_image_files(params[:id])
           if !@file_list.empty?
             @object_id = params[:id]
-            @zip = true
-            # send_content
-            send_zipped_content # experimenting
+            send_zipped_content
           else
             not_found
           end
@@ -38,18 +35,6 @@ module CommonwealthVlrEngine
     end
 
     protected
-
-    def send_zipped_content
-      files_array = []
-      @file_list.each_with_index do |file,index|
-        params[:id] = file[:id] # hack this so file_name returns correct value
-        @solr_document = file
-        files_array << [file_url, "#{(index+1).to_s}_#{file_name_with_extension}"]
-      end
-      params[:id] = @object_id # reset
-      file_mappings = files_array.lazy.map { |url, path| [open(url), path] }
-      zipline(file_mappings, "#{file_name}.zip")
-    end
 
     # Handle the HTTP show request
     def send_content
@@ -64,21 +49,16 @@ module CommonwealthVlrEngine
     end
 
     # send multiple files as a zip archive
-    def zip_stream
-      zip_to_send = Zip::OutputStream.write_buffer do |stream|
-        @file_list.each_with_index do |file,index|
-          params[:id] = file[:id] # hack this so file_name returns correct value
-          @solr_document = file
-          remote_file_response = Typhoeus::Request.get(file_url)
-          unless remote_file_response.code == 404
-            stream.put_next_entry("#{(index+1).to_s}_#{file_name_with_extension}")
-            stream.write(remote_file_response.body)
-          end
-        end
+    def send_zipped_content
+      files_array = []
+      @file_list.each_with_index do |file,index|
+        params[:id] = file[:id] # hack this so file_name returns correct value
+        @solr_document = file
+        files_array << [file_url, "#{(index+1).to_s}_#{file_name_with_extension}"]
       end
-      zip_to_send.rewind
       params[:id] = @object_id # reset
-      zip_to_send
+      file_mappings = files_array.lazy.map { |url, path| [open(url), path] }
+      zipline(file_mappings, "#{file_name}.zip")
     end
 
     # render an HTTP HEAD response
@@ -89,13 +69,7 @@ module CommonwealthVlrEngine
 
     # Create some headers for the datastream
     def content_options
-      filename_for_download = @zip ? "#{file_name}.zip" : file_name_with_extension
-      { disposition: 'attachment', type: mime_type, filename: filename_for_download }
-    end
-
-    # the content to be streamed
-    def content_to_send
-      @zip ? zip_stream : file_stream(file_url)
+      { disposition: 'attachment', type: mime_type, filename: file_name_with_extension }
     end
 
     # returns a Fedora datastream url or IIIF url
@@ -126,15 +100,11 @@ module CommonwealthVlrEngine
     end
 
     def file_size
-      if params[:datastream_id] == 'accessFull' || @zip
-        return false
-      else
-        JSON.parse(@solr_document[:object_profile_ssm].first)["datastreams"][params[:datastream_id]]["dsSize"]
-      end
+      return false if params[:datastream_id] == 'accessFull'
+      JSON.parse(@solr_document[:object_profile_ssm].first)["datastreams"][params[:datastream_id]]["dsSize"]
     end
 
     def mime_type
-      return 'application/zip' if @zip
       if params[:datastream_id] == 'productionMaster'
         @solr_document[:mime_type_tesim].first
       else
@@ -158,7 +128,7 @@ module CommonwealthVlrEngine
     def send_file_contents
       self.status = 200
       prepare_file_headers
-      stream_body content_to_send
+      stream_body file_stream(file_url)
     end
 
     # render an HTTP Range response
