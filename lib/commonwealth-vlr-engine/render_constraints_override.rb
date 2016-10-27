903f5ff7fbd7b4af4ebb44ed195f5e6773fab8fe
Eben English <eenglish@bpl.org>
2016-10-27 10:36:29 -0400

    render contraints for advanced search date range query

--- lib/commonwealth-vlr-engine/render_constraints_override.rb
+++ lib/commonwealth-vlr-engine/render_constraints_override.rb
@@ -24,9 +24,29 @@ module CommonwealthVlrEngine
       super
     end
 
-    # override to add method to show constraint for 'more like this' search
+    # override to add methods to show constraints for 'more like this' and advanced search date range
     def render_constraints(localized_params = params)
-      render_mlt_query(localized_params) + super
+      render_mlt_query(localized_params) + render_advanced_date_query(localized_params) + super
+    end
+
+    # Render the advanced search date query constraints
+    def render_advanced_date_query(localized_params = params)
+      # So simple don't need a view template, we can just do it here.
+      return ''.html_safe if localized_params[:date_start].blank? && localized_params[:date_end].blank?
+
+      date_constraint = if localized_params[:date_start].blank?
+                          "before #{localized_params[:date_end]}"
+                        elsif localized_params[:date_end].blank?
+                          "after #{localized_params[:date_start]}"
+                        else
+                          "#{localized_params[:date_start]}-#{localized_params[:date_end]}"
+                        end
+      render_constraint_element(t('blacklight.advanced_search.constraints.date'),
+                                date_constraint,
+                                :classes => ['date_range'],
+                                :remove => remove_constraint_url(localized_params.merge(date_start: nil,
+                                                                                        date_end: nil,
+                                                                                        :action=>'index')))
     end
 
     # Render the 'more like this' query constraints
