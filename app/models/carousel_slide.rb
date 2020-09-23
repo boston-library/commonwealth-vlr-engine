ae11c98ce757d554a6e603c8da0a7974507bf24b
Eben English <eben.english@gmail.com>
2020-09-23 15:53:08 -0400

    styles for pages#home carousel slides

--- app/models/carousel_slide.rb
+++ app/models/carousel_slide.rb
@@ -1,7 +1,4 @@
 class CarouselSlide < ApplicationRecord
-
-  #attr_accessible :context, :image_pid, :institution, :object_pid, :region, :scale, :sequence, :title
-
+  #attr_accessible :context, :image_pid, :institution, :object_pid, :region, :size, :sequence, :title
   #serialize :region, Array
-
 end
