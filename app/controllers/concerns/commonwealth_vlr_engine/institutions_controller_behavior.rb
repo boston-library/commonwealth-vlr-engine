1ef5464129e8e5d97e5ffef952f5fc279148656a
Eben English <eben.english@gmail.com>
2020-11-12 11:16:11 -0500

    updates for responsive styles

--- app/controllers/concerns/commonwealth_vlr_engine/institutions_controller_behavior.rb
+++ app/controllers/concerns/commonwealth_vlr_engine/institutions_controller_behavior.rb
@@ -42,7 +42,7 @@ module CommonwealthVlrEngine
       # get the response for collection objects
       collex_params = { f: { blacklight_config.index.display_type_field => 'Collection',
                              'institution_pid_ssi' => params[:id] },
-                        rows: 500, sort: 'title_info_primary_ssort asc' }
+                        rows: 500, sort: 'title_info_primary_ssort asc, date_start_dtsi asc' }
       collex_search_service = search_service_class.new(config: blacklight_config,
                                                        user_params: collex_params)
       _collex_response, @collex_documents = collex_search_service.search_results
