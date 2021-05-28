3b20146404afbe64b460fd47313a42be233322b5
Eben English <eben.english@gmail.com>
2021-05-28 15:08:40 -0400

    fix iiif search config [skip ci]

--- app/models/concerns/blacklight_iiif_search/search_behavior.rb
+++ app/models/concerns/blacklight_iiif_search/search_behavior.rb
@@ -7,9 +7,11 @@ module BlacklightIiifSearch
     # return a hash with:
     # key:   solr field for image/file to object relationship
     # value: identifier to match
+    # TODO: use blacklight_config.index.display_type_field once
+    # https://github.com/boston-library/blacklight_iiif_search/issues/5 is resolved
     def object_relation_solr_params
       { iiif_config[:object_relation_field] => id,
-        blacklight_config.index.display_type_field => 'Image' }
+        iiif_config[:page_model_field] => 'Image' } # 'curator_model_suffix_ssi'
     end
   end
 end
