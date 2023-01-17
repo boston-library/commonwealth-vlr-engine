bf326cf071c5de1bde3594c071f162eaef9f9687
Eben English <eben.english@gmail.com>
2023-01-17 07:44:18 -0500

    mods to fulltext js [skip ci]

--- app/assets/javascripts/commonwealth-vlr-engine/basic_search.js
+++ app/assets/javascripts/commonwealth-vlr-engine/basic_search.js
@@ -7,11 +7,21 @@ $(function () {
 
 $('#search_field').change(function () {
     selectedOption = $(this).val();
+    checkbox = $('#fulltext_checkbox');
     if (selectedOption !== "all_fields") {
+        was_checked = checkbox.prop('checked');
+        if (was_checked == true) {
+            $('#fulltext_info').popover('show');
+        }
         $('#fulltext_checkbox_label').css("color", "lightgray");
-        $('#fulltext_checkbox').prop({'checked': false, 'disabled': true});
+        checkbox.prop({'checked': false, 'disabled': true});
+        if (was_checked == true) {
+            setTimeout(() => {
+                $('#fulltext_info').popover('hide');
+            }, 2000)
+        }
     } else {
         $('#fulltext_checkbox_label').css("color", "unset");
-        $('#fulltext_checkbox').prop({'disabled': false});
+        checkbox.prop({'disabled': false});
     }
 }).change();
