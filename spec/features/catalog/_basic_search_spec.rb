c7c37a5960955674d2423691089addad705739dc
Eben English <eben.english@gmail.com>
2023-06-16 16:51:25 -0400

    add #id to search forms for easier tracking [skip ci]

--- spec/features/catalog/_basic_search_spec.rb
+++ spec/features/catalog/_basic_search_spec.rb
@@ -8,7 +8,7 @@ describe 'basic_search', js: true do
   end
 
   it 'shows correct results after running a fielded search' do
-    within '#basic_search_form' do
+    within '#basic_search_form_wrapper' do
       select('Title', from: 'search_field')
       fill_in 'q', with: 'parking'
       click_button('search')
@@ -19,7 +19,7 @@ describe 'basic_search', js: true do
   end
 
   it 'shows correct results after running a full-text search' do
-    within '#basic_search_form' do
+    within '#basic_search_form_wrapper' do
       find('#fulltext_checkbox').click
       fill_in 'q', with: 'assistance'
       click_button('search')
@@ -30,7 +30,7 @@ describe 'basic_search', js: true do
   end
 
   it 'unchecks the full-text checkbox when a non-full-text index is selected' do
-    within '#basic_search_form' do
+    within '#basic_search_form_wrapper' do
       find('#fulltext_checkbox').click
       select('Title', from: 'search_field')
       expect(page).to_not have_checked_field('fulltext_checkbox')
