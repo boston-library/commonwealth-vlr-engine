a3ec08074dce0017ec44a9d824b777c9b7fc26a2
Jean Will <jwill@bpl.org>
2024-01-02 15:49:23 -0500

    Moved brands color to css

--- app/helpers/commonwealth_vlr_engine/show_tools_helper_behavior.rb
+++ app/helpers/commonwealth_vlr_engine/show_tools_helper_behavior.rb
@@ -8,31 +8,26 @@ module CommonwealthVlrEngine
         {
           name: 'Facebook',
           icon: 'facebook-square',
-          color: '#1877f2',
           url: "https://www.facebook.com/sharer/sharer.php?u=#{url}"
         },
         {
           name: 'Pinterest',
           icon: 'pinterest-square',
-          color: '#e60023',
           url: "https://pinterest.com/pin/create/link/?url=#{url}"
         },
         {
           name: 'X',
           icon: 'square-x-twitter',
-          color: '#000000',
           url: "https://twitter.com/intent/tweet?text=#{url}"
         },
         {
           name: 'Reddit',
           icon: 'reddit-square',
-          color: '#ff4500',
           url: "https://www.reddit.com/submit?url=#{url}"
         },
         {
           name: 'Tumblr',
           icon: 'tumblr-square',
-          color: '#35465c',
           url: "https://www.tumblr.com/share/link?url=#{url}"
         }
       ]
--- spec/helpers/show_tools_helper_behavior_spec.rb
+++ spec/helpers/show_tools_helper_behavior_spec.rb
@@ -8,7 +8,7 @@ describe CommonwealthVlrEngine::ShowToolsHelperBehavior do
   describe '#social_sharing_links' do
     it 'returns an array of hashes with the correct structure' do
       helper.social_sharing_links(document).each do |link|
-        expect(link.keys).to contain_exactly(:name, :icon, :color, :url)
+        expect(link.keys).to contain_exactly(:name, :icon, :url)
         expect(link[:url]).to include(document['identifier_uri_ss'])
       end
     end
