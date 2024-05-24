9a828ca0d7a1e7563d1b39689df040fcb44bbe4b
Eben English <eben.english@gmail.com>
2024-05-24 12:57:20 -0400

    refactor #render_blog_feed (fixes #142)

--- spec/helpers/pages_helper_spec.rb
+++ spec/helpers/pages_helper_spec.rb
@@ -2,14 +2,14 @@
 
 require 'rails_helper'
 
-describe PagesHelper do
+describe PagesHelper, :vcr do
   describe '#render_blog_feed' do
-    let(:rendered_feed) { helper.render_blog_feed(File.open(File.expand_path(File.join('..', '..', 'fixtures', 'sample_rss_feed.xml'), __FILE__))) }
+    let(:rendered_feed) { helper.render_blog_feed('https://blog.digitalcommonwealth.org/?feed=rss2') }
 
     it 'creates the correct HTML content' do
       expect(rendered_feed).to include('<ul class="feed_items">')
       expect(rendered_feed).to include('<li class="feed_item">')
-      expect(rendered_feed).to include('href="http://blog.somelibrary.org/?p=942">Event 1')
+      expect(rendered_feed).to include('href="https://blog.digitalcommonwealth.org/?p=2949">DC3 Repository System: An Overview')
     end
 
     it 'has 4 items' do
