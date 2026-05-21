b2ef13ffe7bb42b536c7cae44972cdec0e5a0666
Eben English <eben.english@gmail.com>
2026-05-21 14:27:16 -0400

    update AzLinksComponent

--- spec/components/commonwealth_vlr_engine/az_links_component_spec.rb
+++ spec/components/commonwealth_vlr_engine/az_links_component_spec.rb
@@ -8,7 +8,8 @@ RSpec.describe CommonwealthVlrEngine::AzLinksComponent, type: :component do
   it 'renders the component' do
     render_inline(described_class.new(context: context_arg))
 
-    expect(page).to have_selector('.az_link', count: 27)
+    expect(page).to have_selector('.az_active', count: 1)
+    expect(page).to have_selector('.az_link', count: 26)
     expect(page).to have_link 'A', href: "/#{context_arg}?starts_with=A"
   end
 end
